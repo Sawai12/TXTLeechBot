336e2fcf68285b8904bb2b5631bec364bc9a0b48
Sawai12 <109336735+Sawai12@users.noreply.github.com>
2024-01-18 15:36:03 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 22439323
-api_hash = "e0e203c8be2c2c58b04d0c59fc82f507"
-bot_token = "6769775461:AAHKX7yauyaBxnClEzOE3PC94S5wtCg2Gm8"
+api_id = 27412454
+api_hash = "9a657988cb949b70a313e0c84f14207e"
+bot_token = "6632666376:AAH2Ocb2IGjOJpPlk2BmhDQJxXyrEsRRipE"
 
